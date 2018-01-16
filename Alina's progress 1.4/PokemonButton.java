0f1ce253508d8d917e7723cbbb443df3c63ee02c
kravchenkoalina <340936830@gapps.yrdsb.ca>
2018-01-16 13:24:22 -0500

    combined with alex's code

---
+++ Alina's progress 1.4/PokemonButton.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * This subclass is used to make buttons that, when pressed, instantiate a game with
+ * the speed that is assigned to each given mode. Passing it a different mode changes 
+ * the speed of the game that starts when the specific play button is pressed.
+ * 
+ * @author Alex, Kathy, Alina 
+ * @version November 9th, 2017
+ */
+public class PokemonButton extends Button
+{
+    // constructor makes a button for a specific mode and assigns the main game speed
+    public PokemonButton() {
+        super(); // get functionality of superclass
+        //setImage(new GreenfootImage("AttackButton.png")); // set image to the button appropriate to the mode
+        //getImage().scale(400, 250);
+        x = q2X;
+        y = q2Y;
+        name = "pokemon";
+        setImage(new GreenfootImage(name + "Button.png"));
+    }   
+
+    public void act() {
+        super.act();
+    }
+
+    // this method is executed in the parent's act method
+    public void select() {
+        Party party = new Party();
+        world.addObject(party, world.getWidth()/2, world.getHeight()/2);
+
+        // specific pokemon change stuff
+        world.removeObjects(world.getObjects(FightButton.class)); // have to remove this button LAST
+        world.removeObjects(world.getObjects(RunButton.class)); 
+        world.removeObjects(world.getObjects(BagButton.class)); 
+        world.removeObjects(world.getObjects(Selection.class)); 
+        world.removeObjects(world.getObjects(PokemonButton.class)); 
+    }
+}
---
+++ Alina's progress 1.4/Turns.java
@@ -0,0 +1,12 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Turns extends Actor
+{
+    public static Pokemon player, enemy;
+
+    boolean init = true;
+    public void act() {
+    }
+}
